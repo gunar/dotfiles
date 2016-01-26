df3b27812c65e0359c3b46c7485519d2753c4706
Gunar C. Gessner <gunar@gunargessner.com>
2016-01-26 19:00:24 -0200

    Add termite.conf

--- install.sh
+++ install.sh
@@ -2,3 +2,6 @@ ln -s "$(readlink -f tmux.conf)" ~/.tmux.conf
 
 mkdir -p ~/.config/nvim
 ln -s "$(readlink -f nvim.vim)" ~/.config/nvim/init.vim
+
+mkdir -p ~/.config/termite
+ln -s "$(readlink -f termite.conf)" ~/.config/termite/config
