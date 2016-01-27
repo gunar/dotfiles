fbb18017b67e221d9c0d9259fc96d73a3f46b5b2
Gunar C. Gessner <gunar@gunargessner.com>
2016-01-27 09:02:19 -0200

    Add comments

--- install.sh
+++ install.sh
@@ -1,10 +1,17 @@
+# awesome wm
 ln -s "$(readlink -f awesome)" ~/.config/
 
+# Tmux
 ln -s "$(readlink -f tmux/tmux.conf)" ~/.tmux.conf
+ln -s "$(readlink -f tmux)" ~/.tmux
+
+# Zsh
 ln -s "$(readlink -f zsh/zshrc)" ~/.zshrc
 
+# NeoVim
 mkdir -p ~/.config/nvim
 ln -s "$(readlink -f nvim/nvim.vim)" ~/.config/nvim/init.vim
 
+# Termite
 mkdir -p ~/.config/termite
 ln -s "$(readlink -f termite/termite.conf)" ~/.config/termite/config
