b0f244215de49164dd890001e6671eb00e506706
Gunar C. Gessner <gunar@gunargessner.com>
2016-01-27 08:40:24 -0200

    Move each file to its own directory

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 ln -s "$(readlink -f awesome)" ~/.config/
 
-ln -s "$(readlink -f tmux.conf)" ~/.tmux.conf
-ln -s "$(readlink -f zshrc)" ~/.zshrc
+ln -s "$(readlink -f tmux/tmux.conf)" ~/.tmux.conf
+ln -s "$(readlink -f zsh/zshrc)" ~/.zshrc
 
 mkdir -p ~/.config/nvim
-ln -s "$(readlink -f nvim.vim)" ~/.config/nvim/init.vim
+ln -s "$(readlink -f nvim/nvim.vim)" ~/.config/nvim/init.vim
 
 mkdir -p ~/.config/termite
-ln -s "$(readlink -f termite.conf)" ~/.config/termite/config
+ln -s "$(readlink -f termite/termite.conf)" ~/.config/termite/config
