1e0a9bbdd8cc59fc445b87026d84dbf8c1fb74eb
Gunar C. Gessner <gunar@gunargessner.com>
2016-07-07 20:32:33 -0300

    [awesome] make modkey+scroll switch desktops

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -270,8 +270,8 @@ end
 -- {{{ Mouse bindings
 root.buttons(awful.util.table.join(
     awful.button({ }, 3, function () mymainmenu:toggle() end),
-    awful.button({ }, 4, awful.tag.viewnext),
-    awful.button({ }, 5, awful.tag.viewprev)
+    awful.button({ modkey }, 4, function(t) awful.tag.viewnext(awful.tag.getscreen(t)) end),
+    awful.button({ modkey }, 5, function(t) awful.tag.viewprev(awful.tag.getscreen(t)) end)
 ))
 -- }}}
 
@@ -411,7 +411,9 @@ end
 clientbuttons = awful.util.table.join(
     awful.button({ }, 1, function (c) client.focus = c; c:raise() end),
     awful.button({ modkey }, 1, awful.mouse.client.move),
-    awful.button({ modkey }, 3, awful.mouse.client.resize))
+    awful.button({ modkey }, 3, awful.mouse.client.resize),
+    awful.button({ modkey }, 4, function(t) awful.tag.viewnext(awful.tag.getscreen(t)) end),
+    awful.button({ modkey }, 5, function(t) awful.tag.viewprev(awful.tag.getscreen(t)) end))
 
 -- Set keys
 root.keys(globalkeys)
