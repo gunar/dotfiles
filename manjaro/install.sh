9e1ad9fd0da096f16d626ab47828461a4b997220
Gunar C. Gessner <gunar@gunargessner.com>
2017-02-22 14:29:23 -0300

    [manjaro] add start and enable of services to install.sh

--- manjaro/install.sh
+++ manjaro/install.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 sudo ln -sf "$(readlink -f cgconfig.conf)" /etc/cgconfig.conf
 sudo ln -sf "$(readlink -f cgrules.conf)" /etc/cgrules.conf
+sudo systemctl start cgconfig
+sudo systemctl enable cgconfig
+sudo systemctl start cgrules
+sudo systemctl enable cgrules
