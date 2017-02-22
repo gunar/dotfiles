56ba5916d2a5d7fc8f3b8409835736200a8eccb4
Gunar C. Gessner <gunar@gunargessner.com>
2017-02-22 20:35:06 -0300

    [manjaro] install auto-xflux service

--- manjaro/install.sh
+++ manjaro/install.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
+
+# cgroups
 sudo ln -sf "$(readlink -f cgconfig.conf)" /etc/cgconfig.conf
 sudo ln -sf "$(readlink -f cgrules.conf)" /etc/cgrules.conf
 sudo systemctl start cgconfig
 sudo systemctl enable cgconfig
 sudo systemctl start cgrules
 sudo systemctl enable cgrules
+
+# xflux
+sudo ln -fs "$(readlink -f auto-xflux.service)" /etc/systemd/system/auto-xflux.service
+sudo systemctl start auto-xflux
+sudo systemctl enable auto-xflux
