c1e7b6a5e575a5df29dae90bbf7381217bf00a43
Gunar C. Gessner <gunargessner@gmail.com>
2019-08-21 15:09:35 -0300

    [awesome] fix incognito

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -548,7 +548,7 @@ globalkeys = awful.util.table.join(globalkeys,
     awful.key({ "Control"         }, "q", function () awful.spawn("catfish") end),
     awful.key({ modkey,           }, "o", function () awful.spawn("chromium --disk-cache-dir=/tmp/cache --profile-directory=Default") end),
     awful.key({ modkey, "Control" }, "o", function () awful.spawn("chromium --disk-cache-dir=/tmp/cache --profile-directory=\"Profile 1\"") end),
-    awful.key({ modkey, "Shift" },   "o", function () awful.spawn("chromium --incognito") end),
+    awful.key({ modkey, "Shift"   }, "o", function () awful.spawn("chromium --disk-cache-dir=/tmp/cache --profile-directory=Default --incognito") end),
     awful.key({ modkey,           }, ".", function () awful.spawn("chromium --disk-cache-dir=/tmp/cache --profile-directory=Default https://web.telegram.org") end),
     -- awful.key({                   }, "Print",       function () awful.spawn("xfce4-screenshooter") end),
     -- screenshot
