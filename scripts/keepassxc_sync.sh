9e1ecfbb2beaabfe904afb8a1f5be00b35524735
Gunar C. Gessner <gunargessner@gmail.com>
2020-05-14 19:20:28 +0100

    feat(keepassxc): implement DeadManSnitch

--- scripts/keepassxc_sync.sh
+++ scripts/keepassxc_sync.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
-source "/home/gcg/.keychain/zeno-sh"
-rsync -vrtuv /home/gcg/Passwords.kdbx root@besloor.com:/root/www/Passwords.kdbx
-rsync -vrtuv root@besloor.com:/root/www/Passwords.kdbx /home/gcg/Passwords.kdbx
+. "/home/gcg/.keychain/zeno-sh"
+# The DeadMansSnitch alert is disabled for another day if either of these commands work
+rsync -vrtuv /home/gcg/Passwords.kdbx root@besloor.com:/root/www/Passwords.kdbx && curl https://nosnch.in/17f8b5fc90
+rsync -vrtuv root@besloor.com:/root/www/Passwords.kdbx /home/gcg/Passwords.kdbx && curl https://nosnch.in/17f8b5fc90
