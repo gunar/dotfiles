af6cb79ac595f16e5a4a3d5a7fb94b0e838fe914
Gunar C. Gessner <gunargessner@gmail.com>
2020-06-22 15:21:06 +0100

    fix: apply shellcheck

--- fonts/install.sh
+++ fonts/install.sh
@@ -1,8 +1,8 @@
 # download from somewhere authenticated by rsa key
 git clone git@gitlab.com:gunar/commercial-fonts.git
-cd commercial-fonts
-yes | sudo cp *.ttf /usr/share/fonts/TTF/
+cd commercial-fonts || exit
+yes | sudo cp ./*.ttf /usr/share/fonts/TTF/
 yes | sudo mkdir -p /usr/share/fonts/OTF/
-yes | sudo cp *.otf /usr/share/fonts/OTF/
+yes | sudo cp ./*.otf /usr/share/fonts/OTF/
 cd ..
 rm -rf commercial-fonts
