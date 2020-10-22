8cbab75c91b56ae6116f9501d09567b510e51b44
Gunar C. Gessner <gunargessner@gmail.com>
2020-10-22 12:44:06 +0100

    break(yay): ask for root password at the beginning
    otherwise i'd run the update and leave the computer and
    it would timeout on password input,
    at which point i'd have to start the process all over again

--- manjaro/install2.sh
+++ manjaro/install2.sh
@@ -3,6 +3,8 @@
 
 #packages
 sudo pacman -Syy fasd yay zip extra/unzip extra/keychain community/lxsession upower community/the_silver_searcher extra/imagemagick community/fzf extra/alsa-utils extra/pavucontrol extra/xorg-xinput community/npm 
+# ask for sudo password at the beginning https://github.com/Jguer/yay/issues/1221#issuecomment-606465266
+yay -Y --sudoloop --save
 #sudo pacman -Syy community/xautolock openssh net-tools extra/python-pip extra/python2-pip extra/graphviz extra/gnu-netcat extra/vlc ruby qt4 xdot aria2 aria2 xf86-video-fbdev
 sudo pacman -Syy extra/xorg-server extra/xorg-xinit network-manager net-tools
 sudo pacman -Syy extra/xorg-xbacklight
