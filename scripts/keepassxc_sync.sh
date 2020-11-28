d4e1c1c7f9574299ed3c0dadfd3a06566c990b2e
Gunar C. Gessner <gunargessner@gmail.com>
2020-11-28 20:33:53 +0000

    fix: invalid domain name

--- scripts/keepassxc_sync.sh
+++ scripts/keepassxc_sync.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 . "/home/gcg/.keychain/zeno-sh"
 # The DeadMansSnitch alert is disabled for another day if either of these commands work
-rsync -vrtuv /home/gcg/Passwords.kdbx root@besloor.com:/root/www/Passwords.kdbx && curl https://nosnch.in/17f8b5fc90
-rsync -vrtuv root@besloor.com:/root/www/Passwords.kdbx /home/gcg/Passwords.kdbx && curl https://nosnch.in/17f8b5fc90
+rsync -vrtuv /home/gcg/Passwords.kdbx root@dev.gunar.uk:/root/www/Passwords.kdbx && curl https://nosnch.in/17f8b5fc90
+rsync -vrtuv root@dev.gunar.uk:/root/www/Passwords.kdbx /home/gcg/Passwords.kdbx && curl https://nosnch.in/17f8b5fc90
