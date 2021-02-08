4232eeaf5ad9e2b1a089f483053beecb416b4b25
Gunar C. Gessner <gunargessner@gmail.com>
2021-02-08 12:54:10 +0000

    fix(random-mac): vpn wasn't working

--- manjaro/random-mac/random-mac.sh
+++ manjaro/random-mac/random-mac.sh
@@ -9,6 +9,9 @@ set -xe
 # Log for debugging
 exec 3>&1 1>/tmp/random-mac.log 2>&1
 
-ifconfig "$DEVICE_IFACE" down
-ifconfig "$DEVICE_IFACE" hw ether "$(hexdump -n 6 -ve '1/1 "%.2x "' /dev/random | awk -v a="2,6,a,e" -v r="$RANDOM" 'BEGIN{srand(r);}NR==1{split(a,b,",");r=int(rand()*4+1);printf "%s%s:%s:%s:%s:%s:%s\n",substr($1,0,1),b[r],$2,$3,$4,$5,$6}')"
-ifconfig "$DEVICE_IFACE" up
+if [[ "$DEVICE_IFACE" != *"torguard"* ]]; then
+  ifconfig "$DEVICE_IFACE" down
+  ifconfig "$DEVICE_IFACE" hw ether "$(hexdump -n 6 -ve '1/1 "%.2x "' /dev/random | awk -v a="2,6,a,e" -v r="$RANDOM" 'BEGIN{srand(r);}NR==1{split(a,b,",");r=int(rand()*4+1);printf "%s%s:%s:%s:%s:%s:%s\n",substr($1,0,1),b[r],$2,$3,$4,$5,$6}')"
+  ifconfig "$DEVICE_IFACE" up
+fi
+
